7e8f52e14108acadc506a42323bf1c0bcf4c6573
sheley1998 <sheley1998@gmail.com>
2015-07-01 18:40:44 +0800

    Regular update

--- track_plus_core/track_plus/motion_processor_new.cpp
+++ track_plus_core/track_plus/motion_processor_new.cpp
@@ -1,3 +1,21 @@
+/*
+ * Touch+ Software
+ * Copyright (C) 2015
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Aladdin Free Public License as
+ * published by the Aladdin Enterprises, either version 9 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * Aladdin Free Public License for more details.
+ *
+ * You should have received a copy of the Aladdin Free Public License
+ * along with this program.  If not, see <http://ghostscript.com/doc/8.54/Public.htm>.
+ */
+
 #include "motion_processor_new.h"
 
 bool MotionProcessorNew::compute(Mat& image_in, const string name, const bool visualize)
