a4332c123db96161caaaf3612da8944a595769ca
sheley1998 <sheley1998@gmail.com>
2015-07-06 22:31:34 +0800

    Regular update

--- track_plus_core/track_plus/main.cpp
+++ track_plus_core/track_plus/main.cpp
@@ -89,7 +89,8 @@ LowPassFilter low_pass_filter;
 
 ValueStore value_store;
 
-ToolTrackerMonoProcessor tool_tracker_mono_processor;
+ToolTrackerMonoProcessor tool_tracker_mono_processor0;
+ToolTrackerMonoProcessor tool_tracker_mono_processor1;
 
 const int points_pool_count_max = 1000;
 vector<Point> points_pool[points_pool_count_max];
@@ -486,8 +487,8 @@ void compute()
 		compute_active_light_image(image_small0, image_preprocessed0, image_active_light0);
 		compute_active_light_image(image_small1, image_preprocessed1, image_active_light1);
 
-		tool_tracker_mono_processor.compute(image_active_light0, "0");
-		tool_tracker_mono_processor.compute(image_active_light1, "1");
+		tool_tracker_mono_processor0.compute(image_active_light0, "0");
+		tool_tracker_mono_processor1.compute(image_active_light1, "1");
 
 		imshow("image_active_light0", image_active_light0);
 		imshow("image_active_light1", image_active_light1);
