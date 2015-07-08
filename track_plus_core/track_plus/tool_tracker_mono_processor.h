3b56e3377f499d7de8fc592ef18121592703c9b9
sheley1998 <sheley1998@gmail.com>
2015-07-09 05:38:40 +0800

    CTracker code removed

--- track_plus_core/track_plus/tool_tracker_mono_processor.h
+++ track_plus_core/track_plus/tool_tracker_mono_processor.h
@@ -19,113 +19,18 @@
 #pragma once
 
 #include "value_store.h"
-#include "c_tracker.h"
-#include "permutation.h"
-#include "contour_functions.h"
-
-struct Trace2D
-{
-	float x;
-	float y;
-	int id;
-	int size;
-	int size_total;
-	Point2f point;
-	vector<Point2f> points;
-	float dist;
-
-	Trace2D(Point2f& point_in, int id_in, vector<Point2f>& points_in, float& dist_in, int size_total_in)
-	{
-		x = point_in.x;
-		y = point_in.y;
-		point = point_in;
-		id = id_in;
-		points = points_in;
-		dist = dist_in;
-		size = points_in.size();
-		size_total = size_total_in;
-	}
-};
-
-class ToolTrackerMonoFrame
-{
-private:
-	struct compare_trace_dist_size_ratio
-	{
-		bool operator() (const Trace2D& trace0, const Trace2D& trace1)
-		{
-			float ratio0 = trace0.dist / trace0.size_total;
-			float ratio1 = trace1.dist / trace1.size_total;
-
-			return ratio0 > ratio1;
-		}
-	};
-
-public:
-	vector<Trace2D> trace_vec;
-
-	void sort_traces_by_dist_size_ratio()
-	{
-		sort(trace_vec.begin(), trace_vec.end(), compare_trace_dist_size_ratio());
-	}
-};
-
-struct Rect2D
-{
-	Point pt0;
-	Point pt1;
-	Point pt2;
-	Point pt3;
-	float ratio;
-
-	Rect2D(Point& pt0_in, Point& pt1_in, Point& pt2_in, Point& pt3_in, float& ratio_in)
-	{
-		pt0 = pt0_in;
-		pt1 = pt1_in;
-		pt2 = pt2_in;
-		pt3 = pt3_in;
-		ratio = ratio_in;
-	}
-};
 
 class ToolTrackerMonoProcessor
 {
 public:
-	CTracker tracker = CTracker(0.2, 0.5, 9999, 0, 10);
 	ValueStore value_store;
-
 	Mat image_background_static = Mat(HEIGHT_SMALL, WIDTH_SMALL, CV_8UC1, Scalar(255));
 
-	const int cache_num = 10;
-	const int scan_num = 2;
-	const int permutation_k = 6;
-	int track_id_max = -1;
-
-	vector<ToolTrackerMonoFrame> frame_vec;
-
 	void compute(Mat& image_active_light_in, Mat& image_preprocessed_in, const string name);
-	ToolTrackerMonoFrame get_cached_frame(const int past_frame_num);
 	inline void fill_image_background_static(const int x, const int y, Mat& image_in);
 
 
 private:
-	struct compare_blob_dist_to_pt
-	{
-		Point pt;
-
-		compare_blob_dist_to_pt(Point& pt_in)
-		{
-			pt = pt_in;
-		}
-
-		bool operator() (const BlobNew* blob0, const BlobNew* blob1)
-		{
-			float dist0 = get_distance(blob0->x, blob0->y, pt.x, pt.y);
-			float dist1 = get_distance(blob1->x, blob1->y, pt.x, pt.y);
-
-			return dist0 < dist1;
-		}
-	};
 
 	struct compare_blob_angle
 	{
